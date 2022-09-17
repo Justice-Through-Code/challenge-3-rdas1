1883bcd67c8a3520b5f4418a53fc54ce040cb63f
rdas1 <lilmessi42@gmail.com>
2022-09-17 13:55:32 -0400

    completed challenge 3. Found two issues: 1) in snippet_5(), students are asked to update the print statement to reflect their changes to the expression o = (m != n). But the pre-written print statement already reflects the changes they are supposed to make: print (f'The value of o ({o}) is True since Python is case-sensitive.') The word True should be changed to False before students start the challenge. And 2) Part 1.4 of stocks.py calls for students to print a message that includes the words 'in savings', but the testing program actually looks for the words 'to invest'. The comment should be updated and it should be made clear that the testing program is looking for exact string matches.

--- stocks.py
+++ stocks.py
@@ -8,21 +8,36 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-
+    name = input("What is your name? ")
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
     # and save it into a variable
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
-
+    num_dollars_to_invest = int(input("How much would you like to invest? $"))
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
-    # stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
+    stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ").lower()
     
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
 
     # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
+    desired_stock = ''
+    if stock_name == 'amazon':
+        desired_stock = amazon
+    elif stock_name == 'apple':
+        desired_stock = apple
+    elif stock_name == 'facebook':
+        desired_stock = facebook
+    elif stock_name == 'google':
+        desired_stock = google
+    elif stock_name == 'microsoft':
+        desired_stock = microsoft
+    else:
+        sys.exit('Invalid stock name')
 
+    num_stocks_purchasable = num_dollars_to_invest // desired_stock
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
     # Alex has $5000 in savings and can buy 50 shares of Apple at the current price of $100.
+    print(f'{name} has ${num_dollars_to_invest} to invest and can buy {num_stocks_purchasable} shares of {stock_name.capitalize()} at the current price of ${desired_stock}.')
